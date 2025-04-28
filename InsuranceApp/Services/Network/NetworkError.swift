f9643e4d1ed117c7e94ecd9a643323edd7695476
Ade Dwi Prayitno <adedwip1808@gmail.com>
2025-04-28 23:32:30 +0700

    add: network service

---
+++ InsuranceApp/Services/Network/NetworkError.swift
@@ -0,0 +1,41 @@
+//
+//  NetworkError.swift
+//  InsuranceApp
+//
+//  Created by Ade Dwi Prayitno on 28/04/25.
+//
+
+import Foundation
+
+enum NetworkError: Error, LocalizedError {
+    case middlewareError(code: Int, message: String)
+    case internetError(message: String)
+    case decodingError(message: String)
+    case unAuthorized
+    
+    var localizedDescription: String {
+        switch self {
+        case .middlewareError(_, let message):
+            return message
+        case .internetError:
+            return ""
+        case .decodingError(let message):
+            return message
+        case .unAuthorized:
+            return "unauthorized"
+        }
+    }
+    
+    var isUnAuthorized: Bool {
+        switch self {
+        case .middlewareError:
+            return false
+        case .internetError:
+            return false
+        case .decodingError:
+            return false
+        case .unAuthorized:
+            return true
+        }
+    }
+}
