51cefdfed125bc2cd6cb120ba9bd7ff5f86660f8
Ade Dwi Prayitno <adedwip1808@gmail.com>
2025-04-28 21:11:04 +0700

    Initial Commit

---
+++ InsuranceApp/InsuranceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsuranceAppApp.swift
+//  InsuranceApp
+//
+//  Created by Ade Dwi Prayitno on 28/04/25.
+//
+
+import SwiftUI
+
+@main
+struct InsuranceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
